22a74b0f6d756f8e275a0420fa7320437a690dd2
eramox <eramox@gmail.com>
2016-07-18 23:21:58 +0200

    Add script for jenkins CI and fix of issues

--- runTest.sh
+++ runTest.sh
@@ -1,27 +1,19 @@
 echo "Starting test"
 
+TEST_EXE="gtestexe"
+REPORT_XML="gtestreport.xml"
+REPORT_FOLDER="../"
+
 # moving to test folder
 cd liberamox/build/testing
 
 # Google test
-if [ $run_gtest -eq 1 ]; then
 echo "Running GTest:"
-./completetestlib
+./$TEST_EXE --gtest_output=xml:$REPORT_FOLDER$REPORT_XML
 res_gtest=$?
-fi
 
 cd -
 echo "Test Finished"
 
 exit $res_gtest
-
-if [ "$1" == "" ]
-then
-	echo "no parameter, executing all";
-	run_gtest=1;
-else
-	if [ "$1" == "--gtest" ]
-	then
-		run_gtest=1;
-fi
 
