a5173b9168f9a5939bc76f84a42ee765d2a590d8
eramox <eramox@gmail.com>
2016-07-24 17:26:36 +0200

    Add debug for gtest xml output
    Change-Id: Ia276e2d258162b4cb94a5d57250c5f9d2530ce8a
    Signed-off-by: eramox <eramox@gmail.com>

--- runTest.sh
+++ runTest.sh
@@ -1,19 +1,19 @@
 echo "Starting test"
 
+TEST_FOLDER="liberamox/build/testing"
 TEST_EXE="gtestexe"
 REPORT_XML="gtestreport.xml"
-REPORT_FOLDER="../"
+REPORT_FOLDER="liberamox/build"
 
-# moving to test folder
-cd liberamox/build/testing
+# Look at http://stackoverflow.com/questions/11540633/using-gtest-in-jenkins
 
 # Google test
 echo "Running GTest:"
-./$TEST_EXE --gtest_output=xml:$REPORT_FOLDER$REPORT_XML
+$TEST_FOLDER/$TEST_EXE --gtest_output=xml:$REPORT_FOLDER/$REPORT_XML
 res_gtest=$?
 
-cd -
 echo "Test Finished"
+
 
 exit $res_gtest
 
